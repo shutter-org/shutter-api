900f3a03deec9817cf2e8a073563fa6d6dc05339
Jeremi Girard <jeremigirard@hotmail.com>
2023-03-17 19:56:07 -0400

    feat : overwrite On on imagekit.io

--- shutter_api/MySQL_command/Tools/imagekitio.py
+++ shutter_api/MySQL_command/Tools/imagekitio.py
@@ -3,7 +3,8 @@ from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
 
 
 def addImgToKitio(img, name:str) -> str:
-    upload_response =  IMAGEKIT.upload_file(file=img, file_name=name,options=UploadFileRequestOptions(use_unique_file_name=False))
+    options = UploadFileRequestOptions(use_unique_file_name=False,overwrite_file=True)
+    upload_response =  IMAGEKIT.upload_file(file=img, file_name=name,options=options)
     url = IMAGE_URL_ENDPOINT + upload_response.file_path
     return url
     
